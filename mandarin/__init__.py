b08106273a7de6457932b06b17c21cc661a6a82e
xiaokugua250 <xiaokugua250@hotmail.com>
2021-02-22 17:27:17 +0800

    add code

---
+++ mandarin/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File    :   __init__.py
+@Time    :   2021/02/02 17:57:49
+@Author  :   liang du 
+@Version :   1.0
+@Contact :   liangdu1992@gmail.com
+@License :   (C)Copyright 2021-, MIT
+@Desc    :   None
+'''
+
