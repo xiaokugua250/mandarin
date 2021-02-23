c5f21bd0853bb61b8a2ebe772b58aaaeb75e184b
xiaokugua250 <xiaokugua250@hotmail.com>
2021-02-23 08:57:52 +0800

    add code

---
+++ mandarin/security/scanner/scanner.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   scanner.py
+@Time    :   2021/02/22 20:09:24
+@Author  :   liang du
+@Version :   1.0
+@License :   (C)Copyright 2021-, MIT
+@Contact :   liangdu1992@gmail.com
+@Desc    :   None
+'''
+
+# here put the import lib
+
+from lxml import etree
+from  subprocess import  Popen
+
+import argparse
+import os
+
+def get_ip(machine_name):
+    pass
+
+class Scanner:
+    def  __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    scan = Scanner()
+    print("hello")
+    
